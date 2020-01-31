42c494824bcf8a85f4f93bc3b145b2c053a80df3
NARUTOne <wznarutone326@163.com>
2020-01-31 21:54:29 +0800

    icontext

---
+++ src/components/IconText/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <span>
+    <i :class="iconClassname"></i>
+    <slot></slot>
+  </span>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from "vue-property-decorator";
+
+export default class IconText extends Vue {
+  @Prop(String)
+  public iconClass: string;
+
+  private iconClassname: string = "";
+
+  @Watch("iconClass")
+  private onClassChange(clas: string) {
+    this.iconClassname = clas;
+  }
+
+  mounted() {
+    this.onClassChange(this.iconClassname);
+  }
+}
+</script>
+
+<style scoped></style>
